1f1729e2d077204112758745458f0befc34fd096
Arfon Smith <arfon@github.com>
2014-10-07 20:31:05 -0500

    Flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_class_for(flash_level)
+    case flash_level
+    when 'error'
+      'bg-danger'
+    when 'warning'
+      'bg-warning'
+    when 'notice'
+      'bg-success'
+    else
+      'bg-info'
+    end
+  end
 end
