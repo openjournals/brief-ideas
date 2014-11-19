2f95713a9972bcdc03f30816f23312ef630f63bf
Arfon Smith <arfon@github.com>
2014-11-19 14:34:11 -0600

    Stubbing out background worker.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module BriefIdeas
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths << Rails.root.join('lib')
+
     config.generators do |g|
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, suffix: 'factory', dir: 'spec/factories'
