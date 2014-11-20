8b2cef1f72d9568877442ac9cf572b27ab26fd8a
Arfon Smith <arfon@github.com>
2014-11-19 21:52:50 -0600

    Rest client and worker

--- lib/zenodo_worker.rb
+++ lib/zenodo_worker.rb
@@ -12,14 +12,52 @@ class ZenodoWorker
   end
 
   def create_deposit(idea)
-    puts "CREATING ZENODO DEPOSIT FOR #{idea.sha}"
+    RestClient.post("#{Rails.configuration.zenodo_url}/api/deposit/depositions?access_token=#{Rails.configuration.zenodo_token}", deposit_params(idea), :content_type => :json, :accept => :json){ |response, request, result, &block|
+      case response.code
+      when 201
+        zenodo_response = JSON.parse(response.body)
+        idea.update_attribute(:zenodo_id, zenodo_response['id'])
+        puts "CREATED ZENODO DEPOSIT FOR #{idea.sha}, ZENODO ID #{zenodo_response['id']}"
+      else
+        response.return!(request, result, &block)
+      end
+    }
+  end
+
+  def deposit_params(idea)
+    {
+      :metadata => {
+        :title => idea.title,
+        :upload_type => "publication",
+        :publication_type => "article",
+        :description => idea.formatted_body,
+        :creators => [{:name => idea.user.name, :affiliation => ""}]
+      }
+    }.to_json
   end
 
   def upload_files(idea)
-    puts "UPLOADING FILES FOR #{idea.sha}"
+    RestClient.post("#{Rails.configuration.zenodo_url}/api/deposit/depositions/#{idea.zenodo_id}/files?access_token=#{Rails.configuration.zenodo_token}", { :file => File.new("#{Rails.root}/app/assets/images/unicorn.jpg", 'rb'), :name => "unicorn.jpg", :multipart => true}){ |response, request, result, &block|
+      case response.code
+      when 201
+        zenodo_response = JSON.parse(response.body)
+        puts "UPLOADED FILES FOR #{idea.sha}, ZENODO ID #{zenodo_response['id']}"
+      else
+        response.return!(request, result, &block)
+      end
+    }
   end
 
   def publish!(idea)
-    puts "PUBLISHED! #{idea.sha}"
+    RestClient.post("#{Rails.configuration.zenodo_url}/api/deposit/depositions/#{idea.zenodo_id}/actions/publish?access_token=#{Rails.configuration.zenodo_token}", "", :content_type => :json, :accept => :json){ |response, request, result, &block|
+      case response.code
+      when 202
+        zenodo_response = JSON.parse(response.body)
+        idea.update_attribute(:doi, zenodo_response['doi_url'])
+        puts "PUBLISHED! #{idea.sha}"
+      else
+        response.return!(request, result, &block)
+      end
+    }
   end
 end
