c37c6586033bf336e79779b32e415f3f91c1de02
Arfon Smith <arfon@github.com>
2014-11-21 14:30:20 -0600

    Sidekiq URLs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
+require 'sidekiq/web'
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+  username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDISTOGO_URL'] }
 end
