8eec12bf1749e33ed09ac711a7a8add298cc960d
Arfon Smith <arfon@github.com>
2014-11-21 22:22:25 -0600

    Basic voting specs

--- spec/factories/votes_factory.rb
+++ spec/factories/votes_factory.rb
@@ -1,6 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :vote do
+    user
+    idea
+    created_at  { Time.now }
+    updated_at  { Time.now }
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Vote, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vote do
+  it { should belong_to(:user) }
+  it { should belong_to(:idea) }
+
+  it "should not allow the creator of an idea to vote on it" do
+    user = create(:user)
+    idea = create(:idea, :user => user)
+    user.vote_for!(idea)
+
+    expect(idea.reload.current_vote).to eq(0)
+  end
+
+  it "should not allow the same user to vote twice on an idea" do
+    user = create(:user)
+    idea = create(:idea)
+    user.vote_for!(idea)
+    user.vote_for!(idea)
+
+    expect(idea.reload.current_vote).to eq(1)
+  end
 end
