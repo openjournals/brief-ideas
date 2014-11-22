7c57273bc0025400bf7005ea2cabb9d3cc7cabd0
Arfon Smith <arfon@github.com>
2014-11-21 22:00:56 -0600

    User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe User do
+  it { should have_many(:ideas) }
+  it { should have_many(:votes) }
+
   it "should initialize properly" do
     user = create(:user)
 
@@ -13,5 +16,21 @@ describe User do
     user = create(:user)
 
     expect(user.sha).to eq(user.to_param)
+  end
+
+  it "should know how to create its ORCID id" do
+    user = create(:user, :uid => "0000-0000-0000-1111")
+
+    expect(user.orcid_url).to eq("http://orcid.org/#{user.uid}")
+  end
+
+  # Voting
+  it "should know how to create a vote" do
+    idea = create(:idea)
+    user = create(:user)
+    user.vote_for!(idea)
+
+    expect(idea.reload.current_vote).to eq(1)
+    assert user.voter_for?(idea)
   end
 end
