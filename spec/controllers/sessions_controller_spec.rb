69be736b741bba02e989c24df91a2655b1085d29
Arfon Smith <arfon@github.com>
2014-11-24 20:56:33 -0500

    ORCID name retrieval

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe SessionsController, :type => :controller do
+describe SessionsController do
+  describe "#orcid_name_for" do
+    it "queries the ORCID API for the username based upon the ORCID ID" do
+      # Check spec_helper.rb to see how this is being stubbed
+      user = create(:user, :uid => '0000-0001-7857-2795')
 
+      expect(controller.send(:orcid_name_for, user.uid)).to eq("Einstein, Albert")
+    end
+  end
 end
