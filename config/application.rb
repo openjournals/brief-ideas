30440f59136134a0635603d5123fec1a297c3613
Arfon Smith <arfon@github.com>
2014-11-27 20:45:32 -0600

    Don't use Dotenv in production

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv.overload ".env.#{Rails.env}"
+Dotenv.overload ".env.#{Rails.env}" unless Rails.env.production?
 
 module BriefIdeas
   class Application < Rails::Application
