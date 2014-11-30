edc7e0e926caae91f6e33894de8dc517a7c1849c
Arfon Smith <arfon@github.com>
2014-11-29 18:40:11 -0600

    Moar specs

--- spec/workers/zenodo_worker_spec.rb
+++ spec/workers/zenodo_worker_spec.rb
@@ -4,11 +4,33 @@ Sidekiq::Testing.fake!
 
 # TODO write some more assertions here.
 describe ZenodoWorker do
-  it "should assign a DOI to the idea" do
+  before(:each) do
+    Sidekiq::Worker.clear_all
+    Idea.destroy_all
+  end
+
+  it "should assign a DOI to the idea once processed." do
     idea = build(:idea)
     idea.save
 
     ZenodoWorker.new.perform(idea.sha)
     assert !idea.doi.blank?
+  end
+
+  it "should create the correct number of jobs" do
+    idea = build(:idea)
+    idea.save
+
+    expect {
+      ZenodoWorker.perform_async(idea.sha)
+    }.to change(ZenodoWorker.jobs, :size).by(1)
+  end
+
+  it "should call RestClient three times and Swiftype::Client once" do
+    idea = build(:idea)
+    idea.save
+
+    RestClient.expects(:post).times(3)
+    Swiftype::Client.expects(:create_document).once
   end
 end
