9ff188ea1a885f042e388ebe8970c96cda610ea7
Stuart <stuart@zooniverse.org>
2014-12-17 01:18:01 -0600

    A query to find similar articles. Cobbled together using details in this article : http://factore.ca/blog/259-text-similarity-in-postgresql-and-rails-4

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -83,6 +83,10 @@ class Idea < ActiveRecord::Base
     @redis.smembers("tags-#{Rails.env}")
   end
 
+  def self.similar_ideas(query, limit=4)
+    Idea.find_by_sql ["select * from ideas order by ts_rank_cd(to_tsvector('english', ideas.title || ' ' || ideas.body), replace(plainto_tsquery(?)::text, ' & ', ' | ')::tsquery, 8) DESC Limit ? ", query, limit]
+  end
+
 private
 
   def set_sha
