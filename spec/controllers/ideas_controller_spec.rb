0c502883450a0feded322c9b9044afabc2385270
Stuart <stuart@zooniverse.org>
2014-12-17 03:31:37 -0600

    test to make sure controller is returning correct entries when querying by tag

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -35,6 +35,20 @@ describe IdeasController, :type => :controller do
     end
   end
 
+  describe "GET #index with JSON when specifying tags" do
+    it "should respond with just the tagged objects" do
+      idea1 = create(:idea, :tags => ["space", "dog"])
+      idea2 = create(:idea, :tags => ["space", "cat"])
+
+      get :index, :format => :json, tags: "dog"
+
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+      assert_equal hash_from_json(response.body).first["sha"], idea1.sha
+      assert_equal hash_from_json(response.body).count,  1
+    end
+  end
+
   describe "GET #index with Atom" do
     it "should respond with an Atom feed" do
       idea = create(:idea, :tags => [])
