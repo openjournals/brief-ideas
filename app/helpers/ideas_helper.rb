4daf5fb72a2f97807b8b670bda30c26b0006dfbd
Arfon Smith <arfon@github.com>
2015-01-05 10:37:41 -0800

    Fixes #49

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,13 @@
 module IdeasHelper
+  # TODO adapt formatted_body to accept a length?
+  def truncated_formatted_body(idea)
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SanitizationFilter
+    ]
+
+    truncated_body = idea.body.truncate(250, separator: /\s/) + " " + link_to('continue reading', idea_path(idea))
+    result = pipeline.call(truncated_body)
+    result[:output].to_s.html_safe
+  end
 end
