4b0357c2588ddd0508fb7a33c405bf2006e82495
Arfon Smith <arfon@github.com>
2015-02-21 22:01:25 -0600

    Email notification on stats update

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -2,10 +2,16 @@ class Notification < ActionMailer::Base
   default :from => "noreply@briefideas.org"
 
   EDITOR_EMAILS = ["physicsdavid@gmail.com", "arfon.smith@gmail.com"]
+  SYSTEM_EMAILS = ["arfon.smith@gmail.com"]
+
 
   def submission_email(idea)
     @url  = "http://beta.briefideas.org/ideas/#{idea.sha}"
     @idea = idea
     mail(:to => EDITOR_EMAILS, :subject => "New submission: #{idea.title}")
+  end
+
+  def ratings_email
+    mail(:to => SYSTEM_EMAILS, :subject => "Ratings updated")
   end
 end
--- lib/tasks/ratings.rake
+++ lib/tasks/ratings.rake
@@ -5,4 +5,6 @@ task :update_ratings => :environment do
   Idea.published.find_each do |idea|
     RatingWorker.perform_async(idea.sha)
   end
+
+  Notification.ratings_email.deliver
 end
