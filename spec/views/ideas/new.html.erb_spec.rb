bd410e79993ad336a45c5ec739c67079bce84987
Arfon Smith <arfon@github.com>
2015-02-22 23:25:37 -0600

    Test for email form presence

--- spec/views/ideas/new.html.erb_spec.rb
+++ spec/views/ideas/new.html.erb_spec.rb
@@ -14,5 +14,15 @@ describe 'ideas/new.html.erb' do
       expect(rendered).to have_selector('div.form-group', :count => 3)
       expect(rendered).to have_selector('div.references', :count => 0)
     end
+
+    it "for user without an email should prompt them to add one" do
+      user = create(:no_email_user)
+      allow(view).to receive(:current_user).and_return(user)
+      idea = Idea.new
+      assign(:idea, idea)
+
+      render :template => "ideas/new.html.erb"
+      expect(rendered).to match /Please add an email address to your account before continuing/
+    end
   end
 end
