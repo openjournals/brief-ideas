7f2a57fd41755c5512c2e11f0d7a0c56b37348a8
Arfon Smith <arfon@github.com>
2015-02-23 08:41:34 -0600

    Votes should work for non JS requests too.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,14 @@
 class VotesController < ApplicationController
   before_filter :require_user
-  respond_to :js
+  respond_to :js, :html
 
   def create
     @idea = Idea.find(params[:idea_id])
     current_user.vote_for!(@idea)
+
+    respond_to do |format|
+      format.js
+      format.html { redirect_to(:back, :notice => "Vote created")}
+    end
   end
 end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe VotesController, :type => :controller do
+describe VotesController, :type => :controller do
+  render_views
 
+  describe "JS POST #create" do
+    it "NOT LOGGED IN responds with a redirect" do
+      idea = create(:idea)
+      post :create, :idea_id => idea.id, :format => :js
+
+      expect(response).to be_redirect
+      expect(idea.reload.votes.size).to eq(0)
+    end
+
+    it "LOGGED IN responds with JS" do
+      user = create(:user)
+      allow(controller).to receive_message_chain(:current_user).and_return(user)
+
+      idea = create(:idea)
+      post :create, :idea_id => idea.id, :format => :js
+
+      expect(response).to be_success
+      expect(idea.reload.votes.size).to eq(1)
+    end
+  end
+
+  describe "HTML POST #create" do
+    it "NOT LOGGED IN responds with a redirect" do
+      idea = create(:idea)
+      post :create, :idea_id => idea.id, :format => :html
+
+      expect(response).to be_redirect
+      expect(idea.reload.votes.size).to eq(0)
+    end
+
+    it "LOGGED IN responds with flash" do
+      user = create(:user)
+      allow(controller).to receive_message_chain(:current_user).and_return(user)
+
+      idea = create(:idea)
+      request.env["HTTP_REFERER"] = idea_path(idea)
+
+      post :create, :idea_id => idea.id, :format => :html
+
+      expect(response).to be_redirect # created as HTML redirects
+      expect(idea.reload.votes.size).to eq(1)
+      expect(flash[:notice]).to match /Vote created/
+    end
+  end
 end
