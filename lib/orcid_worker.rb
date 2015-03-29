271c3f11eafef05ade5207bbe09ca2722a74a737
Arfon Smith <arfon@github.com>
2015-03-28 20:29:46 -0500

    Fix ORCID worker

--- lib/orcid_worker.rb
+++ lib/orcid_worker.rb
@@ -12,8 +12,11 @@ class OrcidWorker
   def orcid_name_for(orcid_id)
     data = JSON.parse(open("http://pub.orcid.org/v1.1/#{orcid_id}/orcid-bio", "Accept" => "application/orcid+json").read)
     given_name = data['orcid-profile']['orcid-bio']['personal-details']['given-names']['value']
-    surname = data['orcid-profile']['orcid-bio']['personal-details']['family-name']['value']
-
-    return "#{surname}, #{given_name}"
+    if data['orcid-profile']['orcid-bio']['personal-details'].has_key?('family-name')
+      surname = data['orcid-profile']['orcid-bio']['personal-details']['family-name']['value']
+      return "#{surname}, #{given_name}"
+    else
+      return "#{given_name}"
+    end
   end
 end
