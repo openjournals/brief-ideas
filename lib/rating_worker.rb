f94f0c876455d8d9c9a89feae6b256b0fc33938d
Arfon Smith <arfon@github.com>
2015-04-01 09:29:14 -0500

    Passing the id around

--- lib/rating_worker.rb
+++ lib/rating_worker.rb
@@ -3,10 +3,11 @@ class RatingWorker
 
   def perform(idea_id)
     idea = Idea.find_by_sha(idea_id)
-    calculate_score(idea)
+    calculate_score(idea_id)
   end
 
-  def calculate_score(idea)
+  def calculate_score(idea_id)
+    idea = Idea.find_by_sha(idea_id)
     idea.update_attribute(:score, idea.trending_score)
   end
 end
