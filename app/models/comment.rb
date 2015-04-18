9c25bb07e4f8da7e450c842767f9284c077d1b61
Arfon Smith <arfon@github.com>
2015-04-17 20:36:29 -0500

    Markdown in comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
+require 'html/pipeline'
+
 class Comment < ActiveRecord::Base
   include ActsAsCommentable::Comment
 
@@ -15,5 +17,14 @@ class Comment < ActiveRecord::Base
 
   def notify
     Notification.comment_email(self).deliver
+  end
+
+  def formatted_comment
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SanitizationFilter
+    ]
+    result = pipeline.call(comment)
+    result[:output].to_s
   end
 end
