33e6710aac79783d060d4046a694cba2982da518
Arfon Smith <arfon@github.com>
2015-06-08 16:17:15 -0500

    Testing search result visibility

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe SearchController, :type => :controller do    
+describe SearchController, :type => :controller do
   describe "GET #search with JSON when specifying tags" do
     it "should respond with just the tagged objects" do
-      idea1 = create(:idea, :tags => ["space", "dog"])
-      idea2 = create(:idea, :tags => ["space", "cat"])
+      idea1 = create(:published_idea, :tags => ["space", "dog"])
+      idea2 = create(:published_idea, :tags => ["space", "cat"])
 
       get :search, :format => :json, tags: "dog"
 
@@ -12,6 +12,20 @@ describe SearchController, :type => :controller do
       expect(response.status).to eq(200)
       assert_equal hash_from_json(response.body).first["sha"], idea1.sha
       assert_equal hash_from_json(response.body).count,  1
+    end
+  end
+
+  describe "GET #search with JSON when specifying tags" do
+    it "should respond with only published (visible) ideas" do
+      idea1 = create(:published_idea, :tags => ["space", "dog"])
+      idea2 = create(:published_idea, :tags => ["space", "cat"])
+      idea3 = create(:idea, :tags => ["space", "cat"])
+
+      get :search, :format => :json, tags: "space"
+
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+      assert_equal hash_from_json(response.body).count, 2
     end
   end
 end
