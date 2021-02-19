3d74eee3118ce8fc233519353ac02cc308c572ed
Arfon Smith <arfon@users.noreply.github.com>
2021-02-19 09:52:36 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"],
+    :user_name => 'apikey',
+    :password => ENV["SENDGRID_API"],
     :address => 'smtp.sendgrid.net',
     :domain => 'briefideas.org',
     :port => 587,
